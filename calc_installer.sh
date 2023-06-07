c5924307ff7726b1cdd3f1ad6d07dc69cf7cc714
ciroprogamer86 <132161765+ciroprogamer@users.noreply.github.com>
2023-06-07 19:35:29 +0200

    Update calc_installer.sh

--- calc_installer.sh
+++ calc_installer.sh
@@ -21,6 +21,17 @@ if [ ! $? -eq 0 ]; then
 else
     echo '
     while true; do
+verde='"'"'\033[32m'"'"'
+blanco='"'"'\033[37m'"'"'
+rojo='"'"'\033[31m'"'"'
+azul='"'"'\033[34m'"'"'
+negro='"'"'\033[0;30m'"'"'
+rosa='"'"'\033[38;5;207m'"'"'
+amarillo='"'"'\033[33m'"'"'
+morado='"'"'\033[35m'"'"'
+cian='"'"'\033[1;36m'"'"'
+magenta='"'"'\033[1;35m'"'"'
+uncolor='"'"'\e[0m'"'"'
 	function banner() {
     echo '"'"'
 
@@ -55,8 +66,13 @@ else
         read -e -p ''"Introduce la operación, por ejemplo (2+2)*3.5^2 que da como resultado 49. Los espacios serán ignorados: "' 'operation
         operation=$(echo "$operation" | tr -d '"' '"' '')
         i=$(awk '"'"'BEGIN{print '"'"'$operation'"'"'}'"'"')
+	$i > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
 	echo el resultado es $i
-	sleep 5
+	sleep 3.6
+	else 
+	echo "$verde"\'[\'"$rojo"\'!\'"$verde"\']\'"$rojo"\'SINTAX ERROR\'
+	sleep 1.8
 done' > /usr/bin/calc
     chmod +x /usr/bin/calc
     echo "run calc to start calculator"
